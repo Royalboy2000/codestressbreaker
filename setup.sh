f26280ef1ff22701746d3ddecd90bee01cb735c6
Royalboy2000 <88582814+Royalboy2000@users.noreply.github.com>
2023-08-03 22:58:32 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo su
-sudo apt install npm -y 
-sudo apt install nodejs -y
-npm i
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt install npm -y && sudo apt install nodejs -y && npm i
+
+
